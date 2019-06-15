6c1e1a2a0441a62b21136ee45e707e79bab53f66
tallan groberg <tallan.taven@gmail.com>
2019-06-15 12:09:11 -0600

    api config

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+15.times do
+  d = Department.create(
+    name: Faker::Commerce.department,
+  )
+
+
+
+  10.times do
+    i = Item.create(
+      name: Faker::Commerce.product_name,
+      description: Faker::Lorem.paragraph_by_chars(256, false),
+      price: Faker::Commerce.price(range = 0..1000.00, as_string = false),
+      department_id: d.id
+    )
+  end
+end
+
+puts "seeded items"
